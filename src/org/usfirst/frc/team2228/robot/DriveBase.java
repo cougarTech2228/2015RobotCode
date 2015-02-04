66c53dfc09cd898a0efa95bcf5abe933f312337e
blueForestIcarus <erichspaker2@gmail.com>
2015-02-03 19:33:31 -0500

    voltageFallback

--- src/org/usfirst/frc/team2228/robot/DriveBase.java
+++ src/org/usfirst/frc/team2228/robot/DriveBase.java
@@ -9,35 +9,42 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 
 public class DriveBase{
-    //AdvancedJoystick joy;
-    Joystick joy;
+    AdvancedJoystick joy;
+    //Joystick joy;
 	
     Wheel wheelBL;
     Wheel wheelBR;
-	Wheel wheelFL;
+    Wheel wheelFL;
     Wheel wheelFR;
-    PowerDistributionPanel panel;
 	
-	int COUNTS_PER_REV;
+    public boolean voltageDrive_flg= true;
+    public int FALLBACK_BUTTON = 10;
+    
+    public int COUNTS_PER_REV = 250;
+    
 	
     public DriveBase(int joyPort, int bR, int bL, int fL, int fR){
-    	joy = new Joystick(joyPort);
-    	//joy = new AdvancedJoystick(joyPort);
-    	//joy.defaultMode();
-		//joy.setBypass(true);
-		
-		panel = new PowerDistributionPanel();
+    	//joy = new Joystick(joyPort);
+    	joy = new AdvancedJoystick(joyPort);
+    	joy.defaultMode();
+	joy.setBypass(true);
 		
         wheelFR = new Wheel(fR, COUNTS_PER_REV, "front right");
         wheelFL = new Wheel(fL, COUNTS_PER_REV, "front left");        
         wheelBR = new Wheel(bR, COUNTS_PER_REV, "back right");        
-		wheelBL = new Wheel(bL, COUNTS_PER_REV, "back left");
+	wheelBL = new Wheel(bL, COUNTS_PER_REV, "back left");
 		
-		wheelFL.setInvert(true);
-		wheelBL.setInvert(true);
+	wheelFL.setInvert(true);
+	wheelBL.setInvert(true);
+	
+	voltageDrive_flg = false;
     }
    
-    public void mecanumDrive(double time){    	
+    public void mecanumDrive(double time){
+    	if(joy.getButton(FALLBACK_BUTTON) == true){
+    		voltageDrive = true;
+    	}
+    	
     	double lMag;//linear magnitude (-1 to 1)
     	double dir;//direction (in radians)
     	double rotate;//rotational magnitude (-1 to 1)
@@ -59,19 +66,17 @@ public class DriveBase{
     	v4 = lMag*Math.sin(dir + (Math.PI/4)) - rotate;
     	
     	//push new motor speed to the Jaguars
-    	/*
-    	wheelFR.drive(v1*maxSpeed);
-		wheelFL.drive(v2*maxSpeed);
-		wheelBR.drive(v3*maxSpeed);
-		wheelBL.drive(v4*maxSpeed);
-		*/
     	
-    	wheelFR.setVoltage(v1);
+    	if( ! voltageDrive_flg){
+    		wheelFR.setSpeed(v1*maxSpeed);
+		wheelFL.setSpeed(v2*maxSpeed);
+		wheelBR.setSpeed(v3*maxSpeed);
+		wheelBL.setSpeed(v4*maxSpeed);
+    	}else{
+	    	wheelFR.setVoltage(v1);
 		wheelFL.setVoltage(v2);
 		wheelBR.setVoltage(v3);
 		wheelBL.setVoltage(v4);
-		
-		wheelBL.update(time);
-		
+    	}
     }
 }
